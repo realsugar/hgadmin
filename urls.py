e7f83ef76f8e01812be61d4810ccc9ce43b554c6
realsugar <sakharov@mlsdev.com>
2013-08-05 23:48:10 +0300

    View Templates ported to Django 1.5

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {'template': 'index.html'}),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     (r'^developers/', include('developers.urls')),
     (r'^projects/', include('projects.urls')),
 )
